aa8c157c12bc3cbbcb20902759d24878067caa2c
Rolando L Gonzales <rolandolloyd@gmail.com>
2024-07-22 04:00:58 -0400

    07.22.2024

--- src/assets/data/styleguideData.js
+++ src/assets/data/styleguideData.js
@@ -2,7 +2,7 @@ import { collection, doc, setDoc } from "firebase/firestore";
 
 const sectionRef = collection(db, "styleguideSection");
 
-await setDoc(doc(sectionRef, "SF"), {
-    name: "San Francisco", state: "CA", country: "USA",
-    capital: false, population: 860000,
-    regions: ["west_coast", "norcal"] });
+// await setDoc(doc(sectionRef, "SF"), {
+//     name: "San Francisco", state: "CA", country: "USA",
+//     capital: false, population: 860000,
+//     regions: ["west_coast", "norcal"] });
